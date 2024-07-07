ecd7ae0b3535825c025616650520bf22dd15d07a
Sadykova-11309 <144334653+Sadykova-11309@users.noreply.github.com>
2024-07-07 17:13:33 +0300

    added repository with malls and shops information

---
+++ app/src/main/java/com/example/mallfinder/repository/Shop.kt
@@ -0,0 +1,10 @@
+package com.example.mallfinder.repository
+
+data class Shop(
+    val shopID: Int,
+    val shopName: String,
+    val shopCategory: String,
+    val shopLink: String,
+    val shopInfo: String,
+    val shopURL: String
+)
