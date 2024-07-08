fce361f8f317fe644afef582154d6854406c7875
Sadykova-11309 <144334653+Sadykova-11309@users.noreply.github.com>
2024-07-08 18:08:27 +0300

    added Alert Dialog for filtration

--- app/src/main/java/com/example/mallfinder/bottomnavigation/MallsListFragment.kt
+++ app/src/main/java/com/example/mallfinder/bottomnavigation/MallsListFragment.kt
@@ -1,5 +1,7 @@
 package com.example.mallfinder.bottomnavigation
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
@@ -16,6 +18,24 @@ class MallsListFragment : Fragment(R.layout.fragment_malls_list) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMallsListBinding.bind(view)
+
+        binding?.run {
+            ivFilter.setOnClickListener {
+                val dialog = AlertDialog.Builder(context)
+                dialog.setTitle("Фильтр")
+                dialog.setPositiveButton(
+                    "Применить Фильтр",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        dialog.dismiss()
+                    })
+                dialog.setNegativeButton(
+                    "Очистить Фильтр",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        dialog.dismiss()
+                    })
+                dialog.show()
+            }
+        }
     }
 
     override fun onDestroyView() {
