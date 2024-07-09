fbba6773de2c570fd477d98c386e9512dbbbb385
yakushevaaa <yakushevaa.itis@gmail.com>
2024-07-09 18:19:28 +0300

    Add mall list fragment

--- app/src/main/java/com/example/mallfinder/bottomnavigation/MallsListFragment.kt
+++ app/src/main/java/com/example/mallfinder/bottomnavigation/MallsListFragment.kt
@@ -5,8 +5,14 @@ import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.example.mallfinder.R
 import com.example.mallfinder.databinding.FragmentMallsListBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.mallfinder.data.MallRepository
+import com.example.mallfinder.detail.MallDetailFragment
+import com.example.mallfinder.mallList.MallAdapter
 
 // Экран 1
 // Фрагмент элемента BottomNavigation "ТЦ"
@@ -14,6 +20,7 @@ import com.example.mallfinder.databinding.FragmentMallsListBinding
 class MallsListFragment : Fragment(R.layout.fragment_malls_list) {
 
     private var binding: FragmentMallsListBinding? = null
+    private var adapter : MallAdapter? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,10 +43,34 @@ class MallsListFragment : Fragment(R.layout.fragment_malls_list) {
                 dialog.show()
             }
         }
+        initAdapter()
+
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
     }
+
+
+    private fun initAdapter(){
+        adapter = MallAdapter(
+                list = MallRepository.malls.values.toList(),
+                glide = Glide.with(this),
+                onClick = {
+                    findNavController().navigate(
+                        resId = R.id.action_mallFragment_to_mallDetailFragment,
+                        args = MallDetailFragment.bundle(
+                            mallName = it.name
+                        )
+                    )
+                }
+            )
+        binding?.run{
+            rvMall.adapter = adapter
+            rvMall.layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
 }
